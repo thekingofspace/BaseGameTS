4094acb67f5b151eca44737ae2eb4fd7d7b6f63f
thekingofspace <37231416+thekingofspace@users.noreply.github.com>
2026-08-16 14:46:01 -0700

    Added things I may need later

---
+++ src/PrimaryGame/Flags.ts
@@ -0,0 +1,6 @@
+export enum ConnectionState {
+    Dead,
+    Alive,
+    Idle,
+    Connecting
+}
--- src/PrimaryGame/Items.ts
+++
@@ -1,4 +0,0 @@
-export const Items = {
-	Sword: { Damage: 10, Rarity: "Common" },
-	Bow: { Damage: 7, Rarity: "Rare" },
-} as const;
